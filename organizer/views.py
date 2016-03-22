2c0dff4024308eb684b332750dfa3e7609350266
Camerino <alimjan_abla@yahoo.com>
2016-03-22 17:58:49 +0200

    commit after app url configuration

--- organizer/views.py
+++ organizer/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.http.response import HttpResponse, Http404
 from .models import Tag
+from django.template import Context, loader
 
 def homepage(request):
     tag_list = Tag.objects.all()
-    output = ", ".join([tag.name for tag in tag_list])
-    return HttpResponse(output)
+    return render_to_response('organizer/tag_list.html',
+                              {'tag_list': tag_list})
 
-# Create your views here.
+def tag_detail(request, slug):
+    tag = get_object_or_404(Tag, slug__iexact=slug)
+    return render_to_response('organizer/tag_detail.html',
+                              {'tag': tag})
+
+
--- suorganizer/urls.py
+++ suorganizer/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from organizer import views
+from organizer import urls as organizer_urls
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'suorganizer.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', views.homepage),
+    url(r'^admin/',
+        include(admin.site.urls)),
+    url(r'^', include(organizer_urls)),
 ]
